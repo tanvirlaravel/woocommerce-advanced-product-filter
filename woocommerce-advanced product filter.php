0aa38fd4d38e93f7691dc128d95966053eae9d41
Tanvir <tanvirlaravel@gmail.com>
2024-10-11 12:04:27 +0600

    Ensure WooCommerce is Active

--- woocommerce-advanced product filter.php
+++ woocommerce-advanced product filter.php
@@ -10,3 +10,15 @@ Author: Md Tanvirul Islam
 if ( !defined( 'ABSPATH' ) ) {
     exit;
 }
+
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    
+    // WooCommerce is active, add your custom functions here
+}
+
+
+
+
+
+
+
