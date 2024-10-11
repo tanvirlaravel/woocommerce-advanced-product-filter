dac02bd05834894c9b746bb41316625a82ac7e6b
Tanvir <tanvirlaravel@gmail.com>
2024-10-11 12:03:19 +0600

    initial commit

---
+++ woocommerce-advanced product filter.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Advanced Product Filter
+Description: Adds advanced filtering options for WooCommerce products.
+Version: 1.0
+Author: Md Tanvirul Islam
+*/
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
